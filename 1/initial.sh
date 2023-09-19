bd89e32ca92fb655d23f5751fdda172985be3c33
Seirei <eiseirei@gmail.com>
2023-09-19 17:21:51 +0300

    Update initial.sh

--- 1/initial.sh
+++ 1/initial.sh
@@ -9,7 +9,7 @@ else
 	sudo dnf install -y dnf-utils
 	sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 fi
- echo "Удаление старого пакета Docker"
+ echo "Удаление стороннего пакета Docker"
  sudo dnf remove -y podman runc
  echo "Установка пакета Docker"
  sudo dnf install -y docker-ce docker-ce-cli containerd.io
